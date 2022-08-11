46a46c906fc33b322bec14cdc8c13e3ffcbf2b31
Eduardo <eduandrade.lara@hotmail.com>
2022-08-11 10:13:50 -0300

    adicionando guarda de rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,28 @@
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpComponent } from './signUp/signUp.component';
 import { ModuleWithProviders, NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard'
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo([''])
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
+export class AppRoutingModule {}
+const APP_ROUTES: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AngularFireAuthGuard],
+  },
+  { path: 'signUp', component: SignUpComponent },
+];
 
-}
-const APP_ROUTES : Routes = [
-  { path : '', component : LoginComponent },
-  { path : 'home', component : HomeComponent},
-  { path : 'signUp',component : SignUpComponent}
-]
-
-
-export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(APP_ROUTES);
+export const routing: ModuleWithProviders<RouterModule> =
+  RouterModule.forRoot(APP_ROUTES);
